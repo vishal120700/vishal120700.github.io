360cf04697c78bd26b603e7bdc510abf9c4254a8
Sandesh Arsud <arsudsandesh97@gmail.com>
2025-02-19 14:07:48 +0530

    Updates

---
+++ static/js/952.6c07a675.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkVishal_Pagare=self.webpackChunkVishal_Pagare||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.6c07a675.chunk.js.map
